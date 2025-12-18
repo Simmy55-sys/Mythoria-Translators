450b82826fee4f312ac36dad3ccc8899c751730c
Cyril Ameh <cyrilameh1313@gmail.com>
2025-12-18 21:38:09 +0100

    Removed invalid logo in public folder

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,18 +1,31 @@
 import { LoginForm } from "@/components/page/account/login";
+import { Spinner } from "@/components/ui/spinner";
 import Logo from "@/global/logo";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col">
-        <div className="flex items-center self-center font-medium font-heading text-3xl">
-          <div className="text-primary-foreground font-bold flex items-center justify-center rounded-md">
-            <Logo className="size-20" />
+    <Suspense
+      fallback={
+        <div className="min-h-screen bg-background flex items-center justify-center">
+          <div className="text-center flex items-center gap-2">
+            <Spinner />
+            <p className="text-muted-foreground">Loading...</p>
           </div>
-          {/* Mythoria */}
         </div>
-        <LoginForm />
+      }
+    >
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col">
+          <div className="flex items-center self-center font-medium font-heading text-3xl">
+            <div className="text-primary-foreground font-bold flex items-center justify-center rounded-md">
+              <Logo className="size-20" />
+            </div>
+            {/* Mythoria */}
+          </div>
+          <LoginForm />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
